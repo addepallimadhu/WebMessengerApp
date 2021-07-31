a941da38bba0b7b0e82a3c83de9465edf801e963
addepallimadhu <49113120+addepallimadhu@users.noreply.github.com>
2021-07-31 23:30:51 +0530

    Clean up console logging

--- messenger-ui/src/Utils/RefreshToken.js
+++ messenger-ui/src/Utils/RefreshToken.js
@@ -4,14 +4,14 @@ export const refreshTokenSetup = (res) => {
 
      localStorage.setItem('authToken', res.tokenObj.id_token);
 
-    console.log(res);
-console.log(res.tokenObj.id_token);
+  //  console.log(res);
+//console.log(res.tokenObj.id_token);
   
     const refreshToken = async () => {
       const newAuthRes = await res.reloadAuthResponse();
       refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
-      console.log('newAuthRes:', newAuthRes);
-      console.log('ID TOKEN:', newAuthRes.id_token); 
+   //   console.log('newAuthRes:', newAuthRes);
+   //   console.log('ID TOKEN:', newAuthRes.id_token); 
       // saveUserToken(newAuthRes.access_token);  <-- save new token
     //  localStorage.setItem('authToken', newAuthRes.id_token);
 	 localStorage.setItem('authToken', newAuthRes.id_token);
